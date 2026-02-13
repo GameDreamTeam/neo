4f207467e178ac49e8acc2e6571f32e922176663
giriparus <parusgiri@gmail.com>
2026-02-14 01:43:55 +0530

    fix: remove yield from generate

--- ai/cli.py
+++ ai/cli.py
@@ -1,6 +1,8 @@
 import typer
 from ai.commands.ask import ask
 from ai.commands.chat import chat
+from ai.commands.summarize import summarize
+from ai.commands.explain import explain
 
 no_stream = False
 
@@ -8,6 +10,8 @@ app = typer.Typer()
 
 app.command()(ask)
 app.command()(chat)
+app.command()(summarize)
+app.command()(explain)
 
 if __name__ == "__main__":
     app()
--- ai/llm/ollama_client.py
+++ ai/llm/ollama_client.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Generator
 from ollama import chat, generate
 from ai.llm.base import BaseLLMClient
 from ai.config import load_config
@@ -11,8 +11,9 @@ class OllamaClient(BaseLLMClient):
         self.model = config["model"]
         self.temperature = config["temperature"]
 
-    def chat(self, messages: List[Dict], no_stream) -> str:
+    def chat(self, messages: List[Dict], no_stream) -> str | Generator[str, None, None]:
         try:
+            print("hello")
             shouldStream = not no_stream
             response = chat(
                 model=self.model,
@@ -22,12 +23,16 @@ class OllamaClient(BaseLLMClient):
                 #     "temperature": self.temperature
                 # }
             )
-            return response["message"]["content"]
+            if shouldStream:
+                for chunk in response:
+                  if "message" in chunk:
+                    yield chunk["message"]["content"]
+            else: return response["message"]["content"]
 
         except Exception as e:
             raise RuntimeError(f"Ollama request failed: {e}")
 
-    def generate(self, prompt: str, no_stream) -> str:
+    def generate(self, prompt: str, no_stream) -> str | Generator[str, None, None]:
         try:
             shouldStream = not no_stream
             response = generate(
@@ -38,7 +43,10 @@ class OllamaClient(BaseLLMClient):
                 #     "temperature": self.temperature
                 # }
             )
-            return response["message"]["content"]
+            if shouldStream:
+                for chunk in response:
+                    print(chunk["response"])
+            else: return response["response"]
 
         except Exception as e:
             raise RuntimeError(f"Ollama request failed: {e}")
